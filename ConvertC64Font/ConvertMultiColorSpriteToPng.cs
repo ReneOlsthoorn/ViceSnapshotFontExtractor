4422c87bffffd22dc940401b3ab202c986a9f3ff
Olsthoorn, René (Q.A.) <rene.olsthoorn@politie.nl>
2023-12-08 21:11:08 +0100

    -

--- ConvertC64Font/ConvertMultiColorSpriteToPng.cs
+++ ConvertC64Font/ConvertMultiColorSpriteToPng.cs
@@ -11,6 +11,7 @@ namespace ConvertC64Font
     public class ConvertMultiColorSpriteToPng
     {
         Pixel[] colors = new Pixel[4];
+        const int bytesPerSpriteRow = 1;
         const int bytesPerSprite = 8;           //21*3  
         const int nrSpritesPerRow = 16;         //6
         const int nrWidthPixelsPerSprite = 16;  //16*3 : 24 width * 2 pixels for each point
@@ -19,11 +20,11 @@ namespace ConvertC64Font
         public ConvertMultiColorSpriteToPng(byte[] font, string outputFilename) {
 
             int height = 256*32;
-            var builder = PngBuilder.Create(nrWidthPixelsPerSprite * nrSpritesPerRow, height, false);  //24 * 2 * 6
-            colors[0] = new Pixel(0, 0, 0);
-            colors[1] = new Pixel(255, 0, 0);
-            colors[2] = new Pixel(0, 255, 0);
-            colors[3] = new Pixel(255, 255, 255);
+            var builder = PngBuilder.Create(nrWidthPixelsPerSprite * nrSpritesPerRow, height, false);
+            colors[0] = new Pixel(221, 136, 85);         // color 8: orange
+            colors[3] = new Pixel(0, 0, 0);         // color 0: black
+            colors[2] = new Pixel(255, 255, 255);   // color 1: white
+            colors[1] = new Pixel(255, 119, 119);   // color 10: light red
 
             using var memory = new MemoryStream();
 
@@ -47,17 +48,21 @@ namespace ConvertC64Font
             int col = position % nrSpritesPerRow;
             x = col * nrWidthPixelsPerSprite;
             y = row * nrHeightPixelsPerSprite;
-            for (int i = 0; i < 21; i++)
+
+            for (int i = 0; i < (nrHeightPixelsPerSprite/2); i++)
             {
-                SetSpriteRow(builder, new byte[3] { data[i * 3], data[(i * 3) + 1], data[(i * 3) + 2] }, x, y + (i * 2));
-                SetSpriteRow(builder, new byte[3] { data[i * 3], data[(i * 3) + 1], data[(i * 3) + 2] }, x, y + ((i * 2) + 1));
+                byte[] rowData = new byte[bytesPerSpriteRow];
+                Array.Copy(data, i* bytesPerSpriteRow, rowData, 0, bytesPerSpriteRow);
+
+                SetSpriteRow(builder, rowData, x, y + (i * 2));
+                SetSpriteRow(builder, rowData, x, y + ((i * 2) + 1));
             }
         }
 
         void SetSpriteRow(PngBuilder builder, byte[] data, int x, int y)
         {
             int counter = 0;
-            for (int j = 0; j < 3; j++)
+            for (int j = 0; j < bytesPerSpriteRow; j++)
             {
                 for (int i = 3; i >= 0; i--)
                 {
